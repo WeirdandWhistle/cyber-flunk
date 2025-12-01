9a4c35e38e961549613f0a11d3449fe9b1b88e05
whynotjava <tinycreeperx@gmail.com>
2025-12-01 11:43:06 -0500

    object/class used to contain information about sprite sheets

---
+++ CyberFlunk/src/tiles/SpiteSheet.java
@@ -0,0 +1,13 @@
+package tiles;
+
+public class SpiteSheet {
+	public int width;
+	public int height;
+	public String filePath;
+	public SpiteSheet(String filePath, int width, int height) {
+		this.width = width;
+		this.height = height;
+		this.filePath = filePath;
+	}
+
+}
