40ffc2d734fc26d3c44739c4853877d0bc7b582a
whynotjava <tinycreeperx@gmail.com>
2025-12-01 11:42:31 -0500

    add a tile class/object for my tile based game

---
+++ CyberFlunk/src/tiles/Tile.java
@@ -0,0 +1,13 @@
+package tiles;
+
+import java.awt.image.BufferedImage;
+
+public class Tile {
+
+	public static int LENGTH = 25;
+
+	public boolean collision = false;
+	public int index;
+	public BufferedImage texture;
+
+}
