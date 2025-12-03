a790a08c55013ad4b598f058b279092698d731f5
whynotjava <tinycreeperx@gmail.com>
2025-12-02 21:37:08 -0500

    load map & draw funciton

--- CyberFlunk/src/tiles/TileManager.java
+++ CyberFlunk/src/tiles/TileManager.java
@@ -1,8 +1,13 @@
 package tiles;
 
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.imageio.ImageIO;
@@ -11,6 +16,7 @@ public class TileManager {
 
 	// public ArrayList<Tile> tiles;
 	public HashMap<Integer, Tile> tileMap = new HashMap<Integer, Tile>();
+	public ArrayList<ArrayList<Integer>> map = new ArrayList<>();
 
 	public TileManager() {
 		// tiles = new ArrayList<>();
@@ -19,8 +25,9 @@ public class TileManager {
 	public void loadTileFromSpriteSheet(SpiteSheet ss, int x, int y, boolean collision, int index)
 			throws IOException {
 		BufferedImage fullSheet = ImageIO.read(new File(ss.filePath));
-		BufferedImage texture = fullSheet.getSubimage(x * ss.width, y * ss.height, Tile.LENGTH,
-				Tile.LENGTH);
+		// System.out.println("x+windth " + (x * ss.width + SpiteSheet.LENG));
+		BufferedImage texture = fullSheet.getSubimage(x * ss.width, y * ss.height,
+				SpiteSheet.LENGTH, SpiteSheet.LENGTH);
 
 		Tile tile = new Tile();
 		tile.collision = collision;
@@ -41,6 +48,47 @@ public class TileManager {
 
 		tileMap.put(index, tile);
 
+	}
+
+	public void loadMap(String mapPath) {
+		File file = new File(mapPath);
+		try (BufferedReader read = new BufferedReader(new FileReader(file))) {
+			String line = read.readLine();
+			int column = 0;
+			while (line != null) {
+				String broken[] = line.split(" ");
+
+				map.add(new ArrayList<Integer>());
+				for (int i = 0; i < broken.length; i++) {
+					// System.out.print(String.valueOf(broken[i]) + " ");
+					map.get(column).add(Integer.valueOf(broken[i]));
+				}
+				// System.out.println("");
+
+				column++;
+				line = read.readLine();
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void draw(Graphics2D g2d, int screenX, int screenY) {
+		// System.out.println("--- start ---");
+		for (int column = 0; column < map.size(); column++) {
+			ArrayList<Integer> col = map.get(column);
+			for (int row = 0; row < col.size(); row++) {
+				int index = col.get(row);
+				// System.out.print(index + " ");
+				g2d.drawImage(tileMap.get(index).texture, row * Tile.LENGTH - screenX,
+						column * Tile.LENGTH - screenY, Tile.LENGTH, Tile.LENGTH, null);
+			}
+			// System.out.println("");
+
+		}
 	}
 
 }
