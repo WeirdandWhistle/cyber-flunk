d542c29f0d279d6f60f74095473ec0d3c43fa114
whynotjava <tinycreeperx@gmail.com>
2025-12-02 21:38:54 -0500

    draw player and map. camera limiting code + player loader

--- CyberFlunk/src/main/GameLoop.java
+++ CyberFlunk/src/main/GameLoop.java
@@ -2,23 +2,48 @@ package main;
 
 import java.awt.Graphics2D;
 
+import player.Player;
+import tiles.Tile;
 import windowLib.Draw;
 import windowLib.GameLogic;
 
 public class GameLoop implements Draw, GameLogic {
 
-	public GameLoop() {
+	public SettingsManager sm;
 
+	public int screenX = 0;
+	public int screenY = 0;
+
+	public Player player;
+
+	public GameLoop(SettingsManager sm) {
+		this.sm = sm;
+		player = new Player("res\\player");
 	}
 
 	@Override
 	public void gameloop() {
+		player.move(sm.km, sm.gameWindow.getDeltaTime());
 
+		screenX = (int) (player.getCenter().getX() - sm.gameWindow.buffer.getWidth() / 2);
+		screenY = (int) (player.getCenter().getY() - sm.gameWindow.buffer.getHeight() / 2);
+
+		if (screenX < 0) {
+			screenX = 0;
+		}
+		if (screenY < 0) {
+			screenY = 0;
+		}
+		if (screenY > sm.tm.map.size() * Tile.LENGTH - sm.gameWindow.buffer.getHeight()) {
+			screenY = sm.tm.map.size() * Tile.LENGTH - sm.gameWindow.buffer.getHeight();
+		}
 	}
 
 	@Override
 	public void paint(Graphics2D g2d) {
 
+		sm.tm.draw(g2d, screenX, screenY);
+		player.draw(g2d, screenX, screenY);
 	}
 
 }
