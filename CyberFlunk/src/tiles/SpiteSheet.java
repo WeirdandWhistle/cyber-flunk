7f2c5e9b116809be369f5801d8ae038e01a7b548
whynotjava <tinycreeperx@gmail.com>
2025-12-02 21:36:23 -0500

    defualt size laoding

--- CyberFlunk/src/tiles/SpiteSheet.java
+++ CyberFlunk/src/tiles/SpiteSheet.java
@@ -1,12 +1,19 @@
 package tiles;
 
 public class SpiteSheet {
+	public static int LENGTH = 16;
+
 	public int width;
 	public int height;
 	public String filePath;
 	public SpiteSheet(String filePath, int width, int height) {
 		this.width = width;
 		this.height = height;
+		this.filePath = filePath;
+	}
+	public SpiteSheet(String filePath) {
+		this.width = LENGTH;
+		this.height = LENGTH;
 		this.filePath = filePath;
 	}
 
