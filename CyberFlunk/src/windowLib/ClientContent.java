087400fa38e6a4e8438738422bfafb009e8df37b
whynotjava <tinycreeperx@gmail.com>
2025-12-04 15:19:17 -0500

    FPS, DeltaTime, and overlay shnagigins

--- CyberFlunk/src/windowLib/ClientContent.java
+++ CyberFlunk/src/windowLib/ClientContent.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.util.HashMap;
 
 import javax.swing.JPanel;
 
@@ -15,12 +16,16 @@ public class ClientContent extends JPanel implements Runnable {
 	private Dimension dem;
 	private Dimension drawSize;
 	private Draw painter;
+	private Draw customOverlow = null;
 	private GameLogic gameLogic;
 	public boolean stop;
+	public boolean drawOverlay = true;
 	int frames;
 	long runtime = 0;
 	private long startTime;
 	private double deltaTime = 0.0;
+
+	HashMap<String, Double> fpsData = new HashMap<>();
 
 	public ClientContent(Dimension dem, int frames) {
 		this.frames = frames;
@@ -30,6 +35,10 @@ public class ClientContent extends JPanel implements Runnable {
 		this.requestFocusInWindow();
 		this.setPreferredSize(this.drawSize);
 		buffer = new BufferedImage(dem.width, dem.height, BufferedImage.TYPE_INT_ARGB);
+
+		fpsData.put("DT-avg-10", 0.0);
+		fpsData.put("DT-count-10", 1.0);
+		fpsData.put("DT-total-10", 0.0);
 	}
 
 	public void zoom(Dimension newDrawSize) {
@@ -41,9 +50,11 @@ public class ClientContent extends JPanel implements Runnable {
 
 		Graphics g = this.getGraphics();
 
+		Graphics2D g2d = buffer.createGraphics();
+		drawOverlay(g2d);
+
 		g.drawImage(buffer, 0, 0, drawSize.width, drawSize.height, null);
 		runtime++;
-		Graphics2D g2d = buffer.createGraphics();
 		g2d.setColor(Color.white);
 		g2d.fillRect(0, 0, dem.width, dem.height);
 
@@ -62,7 +73,19 @@ public class ClientContent extends JPanel implements Runnable {
 	}
 	public void update() {
 
-		Graphics2D g2d = buffer.createGraphics();
+		fpsData.put("DT-total-10", fpsData.get("DT-total-10") + getDeltaTime());
+		fpsData.put("DT-count-10", fpsData.get("DT-count-10") + 1.0);
+
+		if (fpsData.get("DT-count-10") == 10) {
+			fpsData.put("DT-avg-10", fpsData.get("DT-total-10") / fpsData.get("DT-count-10"));
+
+			fpsData.put("DT-count-10", 0.0);
+			// fpsData.put("DT-total-10", getDeltaTime());
+			fpsData.put("DT-total-10", 0.0);
+			// System.out.println("--- reset ---");
+		}
+
+		// Graphics2D g2d = buffer.createGraphics();
 		/*
 		 * int sineWave = (int) (Math.sin(Math.toRadians(runtime)) * 255);
 		 * sineWave = Math.abs(sineWave);
@@ -75,13 +98,30 @@ public class ClientContent extends JPanel implements Runnable {
 		runGameLogic();
 		paint();
 		drawToScreen();
+		// System.out.println(fpsData.get("DT-avg-10"));
 	}
-	public double getFPS() {
+	public double getFPSTotal() {
 		return (double) runtime
 				/ (double) ((double) (System.currentTimeMillis() - startTime) / 1000);
 	}
+	public double getFPSDeltaTime() {
+		return 1.0 / getDeltaTime();
+	}
 	public void onceASecond() {
-		System.out.println("FPS: " + Math.round(getFPS()));
+		// System.out.println("FPS: " + Math.round(getFPS()));
+	}
+	public void drawOverlay(Graphics2D g2d) {
+		// Graphics g2d = this.getGraphics();
+		if (customOverlow != null) {
+			// customOverlow.paint(g2d);
+			System.out.println("drawing custom overlay");
+		} else {
+			g2d.setColor(new Color(0, 0, 0, 100));
+			g2d.fillRect(0, 0, 55, 10);
+			g2d.setColor(new Color(0, 200, 0, 200));
+			g2d.drawString(("FPS: " + (Math.round(1 / (fpsData.get("DT-avg-10"))))), 0, 10);
+		}
+		// g2d.dispose();
 	}
 	public double getDeltaTime() {
 		return deltaTime;
@@ -104,6 +144,7 @@ public class ClientContent extends JPanel implements Runnable {
 		double nextDrawTime = System.nanoTime() + drawInterval;
 
 		long lastFrameMillis = System.currentTimeMillis();
+		long lastFrameNano = System.nanoTime();
 
 		startTime = System.currentTimeMillis();
 		while (!stop) {
@@ -121,8 +162,11 @@ public class ClientContent extends JPanel implements Runnable {
 			}
 			nextDrawTime += drawInterval;
 
-			deltaTime = (double) (System.currentTimeMillis() - lastFrameMillis) / 1000;
+			// deltaTime = (double) (System.currentTimeMillis() -
+			// lastFrameMillis) / 1000;
+			deltaTime = (double) (System.nanoTime() - lastFrameNano) / 1E9;
 			lastFrameMillis = System.currentTimeMillis();
+			lastFrameNano = System.nanoTime();
 			update();
 
 		}
@@ -142,6 +186,14 @@ public class ClientContent extends JPanel implements Runnable {
 
 	public void setPainter(Draw aPIDraw) {
 		painter = aPIDraw;
+	}
+
+	public Draw getCustomOverlow() {
+		return customOverlow;
+	}
+
+	public void setCustomOverlow(Draw customOverlow) {
+		this.customOverlow = customOverlow;
 	}
 
 }
