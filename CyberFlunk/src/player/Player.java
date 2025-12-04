72c1ce0c506fe59c35254426f3cf1a22506d0ab3
whynotjava <tinycreeperx@gmail.com>
2025-12-04 15:17:41 -0500

    remove debug collision

--- CyberFlunk/src/player/Player.java
+++ CyberFlunk/src/player/Player.java
@@ -1,6 +1,7 @@
 package player;
 
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.io.File;
 import java.io.IOException;
@@ -8,7 +9,7 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 import entity.Entity;
-import keys.KeyManager;
+import main.SettingsManager;
 import tiles.Tile;
 
 public class Player extends Entity {
@@ -17,7 +18,7 @@ public class Player extends Entity {
 	public static final int walkSpeed = Tile.LENGTH * 4;
 
 	public Player(String folder) {
-		super(new Rectangle(0, 0, Tile.LENGTH, Tile.LENGTH));
+		super(new Rectangle(5 * Tile.LENGTH, 4 * Tile.LENGTH, Tile.LENGTH, Tile.LENGTH));
 		this.folder = folder;
 		try {
 			texture = ImageIO.read(new File(folder + "\\main.png"));
@@ -35,27 +36,27 @@ public class Player extends Entity {
 			e.printStackTrace();
 		}
 	}
-	public void move(KeyManager km, double deltaTime) {
+	public void move(SettingsManager sm, double deltaTime) {
 		double movVector[] = {0.0, 0.0};
 		int moveSpeed = walkSpeed;
-		if (km.poll("shift")) {
+		if (sm.km.poll("shift")) {
 			moveSpeed *= 2;
 		}
 
-		if (km.poll("w")) {
+		if (sm.km.poll("w")) {
 			movVector[1] -= 1;
-		} else if (km.poll("s")) {
+		} else if (sm.km.poll("s")) {
 			movVector[1] += 1;
 		}
-		if (km.poll("d")) {
+		if (sm.km.poll("d")) {
 			movVector[0] += 1;
-		} else if (km.poll("a")) {
+		} else if (sm.km.poll("a")) {
 			movVector[0] -= 1;
 		}
 
 		// normilize
 		if (movVector[0] == 0.0 && movVector[1] == 0.0) {
-			return;
+			// return;
 		}
 		// c^2 = a^2 + b^2 | r/radius = c
 		double r = Math.sqrt(Math.pow(movVector[0], 2) + Math.pow(movVector[1], 2));
@@ -66,13 +67,25 @@ public class Player extends Entity {
 		// System.out.println("DT " + deltaTime + " " + normMoveVec[0] *
 		// deltaTime);
 
-		boudingBox.x += (int) Math.round(normMoveVec[0] * deltaTime);
-		boudingBox.y += (int) Math.round(normMoveVec[1] * deltaTime);
+		int moveX = (int) Math.round(normMoveVec[0] * deltaTime);
+		int moveY = (int) Math.round(normMoveVec[1] * deltaTime);
+
+		if (!collision(sm.tm, new Point(boudingBox.x + moveX, boudingBox.y + moveY))) {
+			boudingBox.x += moveX;
+			boudingBox.y += moveY;
+		}
+
 	}
 
 	public void draw(Graphics2D g2d, int screenX, int screenY) {
 		g2d.drawImage(texture, boudingBox.x - screenX, boudingBox.y - screenY, boudingBox.width,
 				boudingBox.height, null);
+
+		// g2d.setColor(Color.green);
+		// g2d.drawRect(boudingBox.x, boudingBox.y, boudingBox.height,
+		// boudingBox.width);
+
+		// drawHitbox(g2d);
 	}
 
 }
